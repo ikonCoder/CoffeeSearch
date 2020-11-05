53f61393270c7a370b5c3c1b93c8652fa01ae45b
Matthew Joseph <matthewjoseph@Matthews-MBP.hsd1.de.comcast.net>
2020-11-05 00:14:10 -0500

    added query for 'brand' and  extra conditionals

--- ajax.php
+++ ajax.php
@@ -1,12 +1,13 @@
 <?php
-//Including Database configuration file.
+//configs
 include "includes/db_config.php";
+
+
 //Getting value of "search" variable from "script.js".
 if (isset($_POST['search'])) {
-//Search box value assigning to $Name variable.
-   $Name = $_POST['search'];
+   $querySearch = $_POST['search'];
 //Search query.
-   $Query = "SELECT DISTINCT country FROM coffee_test WHERE country LIKE '%$Name%' LIMIT 10";
+   $Query = "SELECT DISTINCT company,country FROM coffee_test WHERE company or country LIKE '%$querySearch%' LIMIT 10";
 //Query execution
    $ExecQuery = MySQLi_query($con, $Query);
 
@@ -14,27 +15,41 @@ if (isset($_POST['search'])) {
 while ($data = MySQLi_fetch_array($ExecQuery)) {
 
 //Creating unordered list to display result.
-   echo '<ul>';
-?>
-   <li onclick='menuItemClick("<?php echo $data['country']; ?>")'>
-      <img style='width:20px' src='images/world.png' />
-      <a> <?php echo $data['country']; ?></a>
-   </li>
+   if($data['country']){
+      echo '<ul>';
+      ?>
+         <li onclick='menuItemClick("<?php echo $data['country']; ?>")'>
+            <img style='width:20px' src='images/world.png' />
+            <a> <?php echo $data['country']; ?></a>
+         </li>
+         
+      <?php
+      echo '</ul>';
+      } 
+   if($data['company']){
+      echo '<ul>';
+      ?>
+         <li onclick='menuItemClick("<?php echo $data['company']; ?>")'>
+            <img style='width:20px' src='images/coffeeCup.png' />
+            <a> <?php echo $data['company']; ?></a>
+         </li>
+         
+      <?php
+      echo '</ul>';
+   }
+   } 
    
-<?php
-   echo '</ul>';
-} 
-      
    //if nothing is returned, echo 0 so js file adjust results box
-   if(mysqli_fetch_array($data) === 0){
+   if(!mysqli_fetch_array($data) === 0){
       echo 0;
    }
 }
+
 if (isset(($_POST)['selected'])) {
    $selectedItem = $_POST['selected'];
    $response = [];
 
-   $Query = "SELECT distinct company,region,altitude,processing_method FROM coffee_test WHERE country LIKE '%$selectedItem%' LIMIT 600";
+   $Query = "SELECT distinct company,region,altitude,processing_method FROM coffee_test WHERE country LIKE '%$selectedItem%' LIMIT 9000";
    $ExecQuery = MySQLi_query($con, $Query);
     
    $result = [];
