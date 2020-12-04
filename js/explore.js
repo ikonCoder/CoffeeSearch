0ecc0d32f5c808ff14d09629f859d4c97da6ae62
Matthew Joseph <matthewjoseph@Matthews-MBP.hsd1.de.comcast.net>
2020-12-03 23:46:03 -0500

    added explore popup when coffee item is selected

---
+++ js/explore.js
@@ -0,0 +1,25 @@
+function buildExplore(data){
+    // let photoArrNum = Math.floor(Math.random() * 3);
+    let cleanData = $.parseJSON(data);
+    console.log(cleanData[0].company);
+
+
+    let str = `
+    <div class='exploreContainer'>
+        <div class='coffeeContainer' id="coffeeBackground1"></div>
+        <div class='coffeeDataContainer'>
+            <div> 
+                <span id="explore_companyName">
+                    ${cleanData[0].company}
+                </span>
+                    <div id="exploreInfo">
+                        <span class="exploreItem">Region: ${cleanData[0].region}</span>
+                        <span class="exploreItem">Altitude: ${cleanData[0].altitude}</span>
+                        <span class="exploreItem">Processing Method: ${cleanData[0].processing_method}</span>
+                    </div>
+            </div>
+        </div>
+    </div>`;
+
+    $.fancybox.open(str)
+}
